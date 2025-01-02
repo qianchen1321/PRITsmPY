7a2185967651c048579d5a74076509f4180f68c6
qianchen1321 <805219532@qq.com>
2025-01-02 16:32:34 +0800

    start in 2025-01-02

--- PYPRmanage/models.py
+++ PYPRmanage/models.py
@@ -22,7 +22,8 @@ class project_orders(models.Model):
         (KPOWERUNIT, '千瓦'),
     ]
 
-    Main_type = models.CharField(max_length=20, null=False)
+    #Purchase_OrderId = models.AutoField(primary_key=True)
+    Main_type = models.CharField(max_length=20, null=False, unique=True, primary_key=True)
     Business_type = models.CharField(max_length=20, null=False)
     Brand = models.CharField(max_length=10, null=False)
     Device_config = models.CharField(max_length=500, null=False)
@@ -30,6 +31,17 @@ class project_orders(models.Model):
     unitType = models.CharField(max_length=2, choices=unit_choice, default=HOSTUNIT)
     Power = models.IntegerField(null=True)
     PowerUnit = models.CharField(max_length=2, choices=power_unit_choice, default=POWERUNIT)
+
+class project_devicelist(models.Model):
+    hostname = models.CharField(max_length=50, null=False, unique=True, primary_key=True)
+    #Main_type = models.CharField(max_length=20, null=False, unique=True)
+    sn = models.CharField(max_length=10, null=False)
+    remark1 = models.CharField(max_length=20, null=True)
+    remark2 = models.CharField(max_length=20, null=True)
+    Main_type = models.ForeignKey(project_orders, on_delete=models.CASCADE)
+
+
+
 
 
 
