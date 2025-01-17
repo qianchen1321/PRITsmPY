b7619f936603afaf6a3f12dcc8d666317d7b31f9
qianchen1321 <805219532@qq.com>
2025-01-17 12:19:28 +0800

    start make make clean app in 20250117 12:19

--- PYPRmanage/admin.py
+++ PYPRmanage/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import *
+
+admin.site.register(project_orders)
+admin.site.register(project_devicelist)
+admin.site.register(DeviceToRackInfo)
