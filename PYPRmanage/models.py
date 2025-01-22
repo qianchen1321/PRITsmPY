34706d430267fc3f7827aaea62342818d5260029
qianchen1321 <805219532@qq.com>
2025-01-22 16:11:57 +0800

    start load data for prepare in 20250122 16:11

--- PYPRmanage/models.py
+++ PYPRmanage/models.py
@@ -1,11 +1,28 @@
 from django.db import models
-
+from django.core.exceptions import ValidationError
+from django.core.validators import MaxValueValidator,MinValueValidator
+from django.utils.translation import gettext_lazy as _
 # Create your models here.
 
+POWERUNIT = 'W'
+KPOWERUNIT = 'KW'
+power_unit_choice = [
+    (POWERUNIT, '瓦'),
+    (KPOWERUNIT, '千瓦'),
+]
+
+
+def max_rackvalue_valid(value, fix):
+    if value < fix:
+        raise ValidationError(
+            _('%(value)s 必须小于机柜高度值'),
+            params={'value': value}
+        )
+
 class project_orders(models.Model):
-    HOSTUNIT = "TA"
-    MEMUNIT = 'TI'
-    BLOCKUIT = 'KU'
+    HOSTUNIT = "TAI"
+    MEMUNIT = 'TIAO'
+    BLOCKUIT = 'KUAI'
     SUBASSEMBLE = 'GE'
 
     unit_choice = [
@@ -15,22 +32,15 @@ class project_orders(models.Model):
         (SUBASSEMBLE,"个"),
     ]
 
-    POWERUNIT = 'w'
-    KPOWERUNIT = 'KW'
-    power_unit_choice = [
-        (POWERUNIT, '瓦'),
-        (KPOWERUNIT, '千瓦'),
-    ]
-
     #Purchase_OrderId = models.AutoField(primary_key=True)
     Main_type = models.CharField(max_length=20, null=False, unique=True, primary_key=True)
     Business_type = models.CharField(max_length=20, null=False)
     Brand = models.CharField(max_length=10, null=False)
     Device_config = models.CharField(max_length=500, null=False)
     Quantity_require = models.IntegerField(null=False)
-    unitType = models.CharField(max_length=2, choices=unit_choice, default=HOSTUNIT)
+    unitType = models.CharField(max_length=5, choices=unit_choice, default=HOSTUNIT)
     Power = models.IntegerField(null=True)
-    PowerUnit = models.CharField(max_length=2, choices=power_unit_choice, default=POWERUNIT)
+    PowerUnit = models.CharField(max_length=10, choices=power_unit_choice, default=POWERUNIT)
 
 class project_devicelist(models.Model):
     hostname = models.CharField(max_length=50, null=False, unique=True, primary_key=True)
@@ -41,10 +51,20 @@ class project_devicelist(models.Model):
     Main_type = models.ForeignKey(project_orders, on_delete=models.CASCADE)
 
 
+class RackInfo(models.Model):
+    Rackid = models.CharField(max_length=20, unique=True, primary_key=True)
+    Power = models.IntegerField(null=True)
+    PowerUnit = models.CharField(max_length=4, choices=power_unit_choice, default=POWERUNIT)
+
+
+
 class DeviceToRackInfo(models.Model):
-    Rackid = models.CharField(max_length=10, null=False, unique=True,primary_key=True)
-    Height = models.IntegerField()
-
-
+    # RackRelateHostId = models.AutoField()
+    Rackid = models.CharField(max_length=10, primary_key=True)
+    hostname = models.CharField(max_length=50, null=False, default="hostname_lost")
+    start_Rackuint =models.DecimalField(max_digits=2, decimal_places=0, null=False, validators=[MaxValueValidator(42, message="the height must below than 42"),
+                                                                              MinValueValidator(1, message="the heigt must be at least 1")], default=0)
+    end_Rackuint =models.DecimalField(max_digits=2, decimal_places=0, null=False, validators=[MaxValueValidator(42, message="the height must below than 42"),
+                                                                              MinValueValidator(1, message="the heigt must be at least 1")], default=0)
 
 
